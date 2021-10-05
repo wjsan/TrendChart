a7391060b51fe2a4072aa73266a7e38df69e3f6e
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-10-04 22:28:23 -0300

    refact: changed main form

--- Test/Program.cs
+++ Test/Program.cs
@@ -15,7 +15,7 @@ namespace Test
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LumiLogger());
         }
     }
 }
