e71381bcc6851f645eaa16d4dbdc17dc3123e20b
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-10-04 22:27:32 -0300

    feat: added form to test serial logger

---
+++ Test/LumiLogger.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Test
+{
+    partial class LumiLogger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trend = new TrendChart.Trend();
+            this.datalogger = new TrendChart.Datalogger(this.components);
+            this.esp8266 = new System.IO.Ports.SerialPort(this.components);
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.trendToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trend
+            // 
+            this.trend.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trend.Location = new System.Drawing.Point(0, 24);
+            this.trend.MoveEnabled = false;
+            this.trend.Name = "trend";
+            this.trend.Size = new System.Drawing.Size(800, 545);
+            this.trend.TabIndex = 3;
+            // 
+            // datalogger
+            // 
+            this.datalogger.Directory = "C:\\dat";
+            this.datalogger.MaxSize = 1000;
+            // 
+            // esp8266
+            // 
+            this.esp8266.BaudRate = 74400;
+            this.esp8266.PortName = "COM10";
+            this.esp8266.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.esp8266_DataReceived);
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trendToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(800, 24);
+            this.menu.TabIndex = 4;
+            this.menu.Text = "menuStrip1";
+            // 
+            // trendToolStripMenuItem
+            // 
+            this.trendToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.clearToolStripMenuItem,
+            this.openDatToolStripMenuItem});
+            this.trendToolStripMenuItem.Name = "trendToolStripMenuItem";
+            this.trendToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.trendToolStripMenuItem.Text = "Trend";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // openDatToolStripMenuItem
+            // 
+            this.openDatToolStripMenuItem.Name = "openDatToolStripMenuItem";
+            this.openDatToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.openDatToolStripMenuItem.Text = "Open Dat";
+            this.openDatToolStripMenuItem.Click += new System.EventHandler(this.openDatToolStripMenuItem_Click);
+            // 
+            // LumiLogger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 569);
+            this.Controls.Add(this.trend);
+            this.Controls.Add(this.menu);
+            this.Name = "LumiLogger";
+            this.Text = "LumiLogger";
+            this.Load += new System.EventHandler(this.LumiLogger_Load);
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TrendChart.Trend trend;
+        private TrendChart.Datalogger datalogger;
+        private System.IO.Ports.SerialPort esp8266;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem trendToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDatToolStripMenuItem;
+    }
+}
---
+++ Test/LumiLogger.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class LumiLogger : Form
+    {
+        public LumiLogger()
+        {
+            InitializeComponent();
+        }
+
+        private void esp8266_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string time = DateTime.Now.ToString("HH:mm:ss.fff");
+
+            var jsonStr = esp8266.ReadLine();
+
+            Console.WriteLine(jsonStr);
+
+            try
+            {
+                var lumiData = JsonConvert.DeserializeObject<LumiData>(jsonStr);
+                var i = 1;
+
+                if (lumiData == null) return;
+                foreach (var cn in lumiData.cnSt)
+                {
+                    trend.Invoke((MethodInvoker) delegate {
+                        trend.AddData($"cn{i}", cn.val, time);
+                    });
+
+                    time = DateTime.Now.ToString("MM/dd/yyyy ") + time;
+                    datalogger.AddData($"cn{i}", cn.val, time);
+
+                    i++;
+                }
+            }
+            catch (Exception)
+            {
+                trend.Invoke((MethodInvoker)delegate {
+                    trend.AddText(jsonStr);
+                });
+            }
+        }
+
+        private void LumiLogger_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            trend.Clear();
+            if(!esp8266.IsOpen) esp8266.Open();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            esp8266.Close();
+            trend.MoveEnabled = true;
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            trend.Clear();
+        }
+
+        private void openDatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var openFile = new OpenFileDialog()
+            {
+                Filter = "Dat Files | *.dat",
+                Multiselect = true,
+                InitialDirectory = "C:\\Dat"
+            };
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                esp8266.Close();
+                trend.OpenFiles(openFile.FileNames);
+            }
+        }
+    }
+
+    public class LumiData
+    {
+        public class CnSt
+        {
+            public int st { get; set; }
+            public int val { get; set; }
+        }
+
+        public string ef { get; set; }
+        public string time { get; set; }
+        public int ntwSt { get; set; }
+        public string mac { get; set; }
+        public string ip { get; set; }
+        public List<CnSt> cnSt { get; set; }
+    }
+
+
+}
